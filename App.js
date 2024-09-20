b99d892e5592ea84bc9da41d8e489eb7130644d4
ShinDoan <hodregon@naver.com>
2024-09-21 03:06:07 +0900

    v0.2.0 login page

--- App.js
+++ App.js
@@ -1,93 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity } from 'react-native';
-import { useFonts } from "expo-font";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import LoginStudentScreen from './src/screens/LoginStudentScreen';
+import LoginDriverScreen from './src/screens/LoginDriverScreen';
 
-import KNU_logoEng from './assets/img/KNU_logoEng_Red.png';
-import KNU_emblem_Red from './assets/img/KNU_emblem_Red.png';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    KNU_TRUTH : require("./assets/font/KNU TRUTH.ttf"),
-  });
-  if (!fontsLoaded) return null;
-
-  // 버튼이 눌렸을 때의 동작
-  const onPressHandler = () => {
-    alert('Button Pressed!');
-  };
-
   return (
-    <View style={styles.container}>
-      {/* 배경 이미지가 UI와 독립적으로 움직일 수 있도록 설정 */}
-      <ImageBackground
-        source={KNU_emblem_Red}
-        style={styles.background}
-      >
-        {/* 투명도 오버레이를 추가 */}
-        <View style={styles.overlay} />
-      </ImageBackground>
-
-      {/* 고정된 UI */}
-      <Image
-        source={KNU_logoEng}
-        style={{ width: 242, height: 70 }}
-      />
-      <Text style={styles.KNU_logo_font}>셔틀버스 시스템</Text>
-
-      <View style={styles.spacer} />
-      
-      {/* 버튼 추가 */}
-      <TouchableOpacity style={styles.button} onPress={onPressHandler}>
-        <Text style={styles.buttonText}>학생 로그인</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={onPressHandler}>
-        <Text style={styles.buttonText}>기사 로그인</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="StudentLogin" component={LoginStudentScreen} />
+        <Stack.Screen name="DriverLogin" component={LoginDriverScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex: 1, // UI를 배경 이미지 위에 위치시킴
-  },
-  KNU_logo_font: {
-    fontSize: 38,
-    fontFamily: "KNU_TRUTH",
-    color: '#000',
-  },
-  background: {
-    position: 'absolute',  // 배경 이미지를 절대 위치로 설정
-    top: 230,  // Y 좌표 위치 (배경 이미지를 이동시킬 위치)
-    left: -210,  // X 좌표 위치
-    width: 580,  // 배경 이미지 너비
-    height: 580,  // 배경 이미지 높이
-    resizeMode: 'cover',  // 이미지 크기 조정 방식
-    zIndex: -1,  // 배경 이미지를 UI 뒤로 보냄
-  },
-  overlay: {
-    ...StyleSheet.absoluteFillObject,  // 부모 요소(배경 이미지)를 채우도록 설정
-    backgroundColor: 'rgba(255, 255, 255, 0.85)',  // 투명도 설정 (50%)
-  },
-  button: {
-    backgroundColor: 'rgba(255, 255, 255, 0.85)', // 버튼 배경색
-    paddingVertical: 40, // 버튼 세로 패딩
-    paddingHorizontal: 80, // 버튼 가로 패딩
-    borderRadius: 10, // 버튼의 둥근 모서리
-    borderWidth: 2, // 테두리 두께
-    borderColor: '#DA2127', // 테두리 색상
-    marginTop: 20, // 텍스트 아래 간격
-  },
-  buttonText: {
-    color: '#DA2127', // 텍스트 색상
-    fontSize: 24, // 텍스트 크기
-    fontWeight: 'normal', // 텍스트 굵기
-  },
-  spacer: {
-    height: 50, // 원하는 높이로 설정
-  },
-});
